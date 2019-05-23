c03043c214b0e09b517a88622e83c4b3fb7e20fd
Ather Tahir <ather.tahir@bentley.com>
2019-05-23 16:00:29 +0500

    initial commit

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var express = require('express'),
 app = express.createServer();
 
-app.configure(function(){
-  app.use(express.static(__dirname + '/dist'));
-});
+
 
 app.get('/home', function(req, res){
   res.send("Hello World!");
 });
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + './dist/index.html');
-});
+
 
 const port = process.env.PORT || 1337;
 app.listen(port);
